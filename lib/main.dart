a5df606f1e34a305d216aa3fb0321bc40a494fd2
YOadYo8120 <y.adil8120@uca.ac.ma>
2026-04-15 18:52:25 +0100

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,8 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  const x = null;
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
